a1f00defd734461f82048bcfbf1bf70c5a43eff8
Samuel Amoah <94200301+lilyoo226@users.noreply.github.com>
2022-10-05 14:11:10 +0000

    fully done

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,10 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/", () => {
-  res.send("Hello");
+const items = ["Buy Lamboghini","Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  //  here
+  let day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  console.log(req.body);
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  res.redirect("/work");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(3000, () => {
